cd4c72e35a0a96348c4385e514165ca270be36ea
cristian <chrisdev2002@gmail.com>
2023-12-02 02:00:12 +0200

    idk

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        path: "/",
+        index: true,
         element: <HomePage />,
       },
       {
@@ -36,57 +36,11 @@ const router = createBrowserRouter([
   },
 ]);
 
-// const initialState = {
-//   data: [],
-//   status: null,
-// };
-
-// function reducer(state, action) {
-//   switch (action.type) {
-//     case "loading":
-//       return { data: state.data, status: "fetchingData" };
-//     case "ready":
-//       return { data: action.payload, status: "dataFetched" };
-//     case "error":
-//       return { data: action.payload, status: "errorFetching" };
-//     default:
-//       throw new Error("Something went wrong!");
-//   }
-// }
-
 function App() {
-  // const [{ data, status }, dispatch] = useReducer(reducer, initialState);
-
-  // useEffect(() => {
-  //   const fetchPost = async () => {
-  //     dispatch({ type: "loading" });
-  //     try {
-  //       const querySnapshot = await getDocs(collection(db, "products"));
-  //       let info = [];
-  //       querySnapshot.forEach((doc) => {
-  //         info.push(doc.data());
-  //       });
-  //       // console.log(info);
-  //       dispatch({ type: "ready", payload: info });
-  //     } catch (err) {
-  //       dispatch({ type: "error" });
-  //     }
-  //   };
-
-  //   fetchPost();
-  // }, []);
-
   return (
     <ProductsProvider>
       <RouterProvider router={router} />
     </ProductsProvider>
-    // <DataContext.Provider
-    //   value={{
-    //     data: data,
-    //     status: status,
-    //   }}
-    // >
-    // </DataContext.Provider>
   );
 }
 
